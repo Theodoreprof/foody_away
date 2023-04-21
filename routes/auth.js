26070807e5e5d21c1fedefce06bf170f33f1d531
Theodore Dimopoulos <tdimopoulosprof@gmail.com>
2023-04-21 17:37:34 +0300

    a

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.send({hi : 'dsadasd'});
+    res.send({hi : 'world'});
 });
 
 module.exports = router;
