46041d5140abbd745754a95d4eaedf70623fa819
Theodore Dimopoulos <tdimopoulosprof@gmail.com>
2023-04-22 11:41:29 +0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,18 @@
 const express = require('express')
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const kes = require('./config/keys');
 
 const router = express.Router()
+
+passport.use( new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: 'auth/google/callback',
+}, (accessTOken) => {
+    console.log(accessTOken);
+}
+));
 
 router.get('/', (req, res) => {
     res.send({hi : 'google'});
