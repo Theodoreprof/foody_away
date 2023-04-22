e952d25e1cfc48b3c4ce1752741542358cc00c37
Theodore Dimopoulos <tdimopoulosprof@gmail.com>
2023-04-22 10:51:20 +0300

    google-oauth-strategy-pack

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 
 const Auth = require('./routes/auth');
 
-app.use('/', Auth)
+app.use('/google/auth', Auth)
+
+app.get('/', (req, res) => {
+    res.send({hi : 'world'});
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
