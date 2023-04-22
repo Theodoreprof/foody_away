a34a7c53ff32a00bf916c614be36f97cda5dbace
Theodore Dimopoulos <tdimopoulosprof@gmail.com>
2023-04-22 12:28:09 +0300

    auth_working

--- routes/auth.js
+++ routes/auth.js
@@ -2,21 +2,19 @@ const express = require('express')
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-const Keys = require('./config/keys');
+require("dotenv").config();
 
 const router = express.Router()
 
 passport.use( new GoogleStrategy({
-    clientID: Keys.googleClientID,
-    clientSecret: Keys.googleClientSecret,
+    clientID: process.env.googleClientID,
+    clientSecret: process.env.googleClientSecret,
     callbackURL: 'auth/google/callback',
 }, (accessTOken) => {
     console.log(accessTOken);
 }
 ));
 
-router.get('/', passport.authenticate('google', {
-    scope: [profile, email]
-}));
+router.get('/', passport.authenticate('google'));
 
 module.exports = router;
